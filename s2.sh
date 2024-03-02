12b0259d2ed9ce36bcd1be6173df387c076ac22f
Zahra8890 <161909079+Zahra8890@users.noreply.github.com>
2024-03-01 19:30:20 -0800

    Add files via upload

---
+++ s2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download the file
+wget https://github.com/Zahra8890/test/archive/refs/heads/main.zip
+
+# Install unzip if not installed
+sudo apt-get install -y unzip
+
+# Unzip the downloaded file
+unzip main.zip
+
+# Change directory to 's'
+cd main
+
+unzip s2.zip
+
+cd s2.zip
+
+# Execute the script with sudo privilege to start
+sudo bash s.sh --start
---
+++ s4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download the file
+wget https://github.com/Zahra8890/test/archive/refs/heads/main.zip
+
+# Install unzip if not installed
+sudo apt-get install -y unzip
+
+# Unzip the downloaded file
+unzip main.zip
+
+# Change directory to 's'
+cd main
+
+unzip s4.zip
+
+cd s4.zip
+
+# Execute the script with sudo privilege to start
+sudo bash s.sh --start
